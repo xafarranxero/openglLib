e5e0996b4b490cc11b4bc7546ed8fcfcf8d52813
Jordi <xafarranxero@hotmail.com>
2016-02-18 04:31:27 +0100

    constructores texture con parametros

--- Texture.hpp
+++ Texture.hpp
@@ -14,6 +14,40 @@ namespace JMT
         public:
             Texture():Texture(sf::Vector2i(GL_REPEAT,GL_REPEAT),sf::Vector2i(GL_LINEAR,GL_LINEAR)){}
 
+            Texture(const char* name,const sf::Vector2i &conf, const sf::Vector2i &conf2):Texture(conf,conf2)
+            {
+                loadFromFile(name);
+            }
+
+            Texture(const sf::Image &image,const sf::Vector2i &conf, const sf::Vector2i &conf2):Texture(conf,conf2)
+            {
+                loadFromImage(image);
+            }
+
+
+            Texture(const void *mem,size_t size, const sf::Vector2i &conf, const sf::Vector2i &conf2):Texture(conf,conf2)
+            {
+                loadFromMemory(mem,size);
+            }
+
+            Texture(const char* name):Texture()
+            {
+                loadFromFile(name);
+            }
+
+            Texture(const sf::Image &image):Texture()
+            {
+                loadFromImage(image);
+            }
+
+
+            Texture(const void *mem,size_t size):Texture()
+            {
+                loadFromMemory(mem,size);
+            }
+
+
+
             Texture(const sf::Vector2i &conf, const sf::Vector2i &conf2)
             {
                 glGenTextures(1,&id);
